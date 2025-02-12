986ab5a213faab5eead5a98992118440f59638ae
Satyajeagtap <satyajeetjagtap17@gmail.com>
2025-02-13 00:34:32 +0530

    Updated token

--- src/constants/Constants.tsx
+++ src/constants/Constants.tsx
@@ -4,7 +4,8 @@ const Constants={
     fqdn:"https://openrouter.ai",
     // token:"Bearer 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"
     apiendpoint:"https://openrouter.ai/api/v1/chat/completions",
-    token:"Bearer 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",
+    // token:"Bearer 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",
+    token:"Bearer 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",
     model :"meta-llama/llama-3.3-70b-instruct"
     // model:"google/gemini-2.0-flash-lite-preview-02-05:free" 
     // model:"qwen/qwen-vl-plus:free"
