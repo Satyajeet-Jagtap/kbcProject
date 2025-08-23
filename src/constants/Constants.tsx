cbdf0c6bca889aa4dc1b2cbed597c2dbdc0f53b2
Satyajeet Jagtap <127824054+Satyajeet-Jagtap@users.noreply.github.com>
2025-08-23 22:39:00 +0530

    Updated token

--- src/constants/Constants.tsx
+++ src/constants/Constants.tsx
@@ -2,7 +2,7 @@
 const Constants={
 
     apiendpoint:"aHR0cHM6Ly9vcGVucm91dGVyLmFpL2FwaS92MS9jaGF0L2NvbXBsZXRpb25z",
-    token :"QmVhcmVyIHNrLW9yLXYxLTY1M2VmZGE3YjA3MTBlMjMxODhiYzMwZDY4Yzg1MzAxMmMwNGZmZjc0ZWFmMmY4ZGU5ZmIyZjY3MjdhNWVlM2U=",
+    token :"QmVhcmVyIHNrLW9yLXYxLTFkOGM2MzUxYTEzMGU0Mzg0NmNkNjc4YmM1NDNkMjFlMThhMDkyYWJmYWQwN2Q0NWJlZjEzMWJmMGQyODgyM2M=",
     model :"bWV0YS1sbGFtYS9sbGFtYS0zLjMtNzBiLWluc3RydWN0"
 
 }
